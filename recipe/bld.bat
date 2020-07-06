bd75a4453a8399a78f2bbfa816b1ff522c84ba5e
Rafael M Mudafort <rafmudaf@gmail.com>
2020-07-06 18:18:43 -0500

    Add support for Windows

---
+++ recipe/bld.bat
@@ -0,0 +1,17 @@
+
+mkdir build
+cd build
+
+cmake -G "MinGW Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+    ..
+if errorlevel 1 exit 1
+
+REM -DBLAS_LIBRARIES=$BLASLIB
+REM -DLAPACK_LIBRARIES=$BLASLIB
+REM cmake .. -G "MinGW Makefiles" -D CMAKE_Fortran_FLAGS="-ffree-line-length-0"
+REM if errorlevel 1 exit 1
+
+cmake --build . --config Debug --target install
+if errorlevel 1 exit 1
