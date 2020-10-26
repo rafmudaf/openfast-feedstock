66d47e7e0d55e159692401474f1115ced99249ca
Rafael M Mudafort <rafmudaf@gmail.com>
2020-10-26 16:15:05 -0500

    Build for Simulink

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,9 @@ cd build
 
 cmake \
     -DDOUBLE_PRECISION=OFF \
+    -DBUILD_OPENFAST_SIMULINK_API=ON \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBDIR=lib \
     ..
 
-make -j"${CPU_COUNT}" install
+make -j"${CPU_COUNT}" openfastlib
